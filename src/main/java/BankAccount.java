cb7e90e36c377bd3680f6061a75ea87e56f3e92c
Geoffrey Challen <challen@illinois.edu>
2018-09-30 16:46:00 -0500

    Fall 2018 Lab5 starter code.

---
+++ src/main/java/BankAccount.java
@@ -0,0 +1,38 @@
+import java.util.Random;
+
+/**
+ * Class implementing a bank account.
+ * <p>
+ * Complete and document this class as part of Lab 5.
+ *
+ * @see <a href="https://cs125.cs.illinois.edu/lab/5/">Lab 5 Description</a>
+ */
+public class BankAccount {
+
+    /*
+     * You may want to use this to distinguish between different kinds of accounts.
+     */
+    public enum BankAccountType {
+        CHECKINGS,
+        SAVINGS,
+        STUDENT,
+        WORKPLACE
+    }
+
+    private int accountNumber;
+    public BankAccountType accountType;
+    private double accountBalance;
+    private String ownerName;
+    public double interestRate;
+    private double interestEarned;
+
+    public BankAccount(final String name, final BankAccountType accountCategory) {
+        /*
+         * Implement this function
+         */
+    }
+
+    /*
+     * Implement getters and setters as appropriate for private variables.
+     */
+}
